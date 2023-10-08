130ee4f988a4df10ce900322821ef14477cb85c4
Emanuel Maia <tavanomanu@gmail.com>
2023-10-08 16:18:51 +0100

    Add basic board displaying functionality

---
+++ Project1/board.pl
@@ -0,0 +1,62 @@
+% 
+% consult('utils.pl').
+
+% Define the initial state of the board as a matrix.
+initial_board([
+    [" 2", " 2", " 2", " 2", " 2", " 2", " 2", " 2"],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+    ["II", "II", "II", "II", "II", "II", "II", "II"]
+]).
+
+% Function to print the board.
+print_board(Board) :-
+    nl,
+    print_board_separator,
+    print_rows(Board, 1).
+
+print_rows([Row | Rest], RowNumber) :-
+    DisplayNumber is 8 - RowNumber,
+    format(' ~w ', [DisplayNumber]),
+    print_row(Row, RowNumber),
+    nl,
+    NextRowNumber is RowNumber + 1,
+    print_other_rows(Rest, NextRowNumber).
+
+print_other_rows([], _) :-
+    print_board_separator,
+    print_bottom_letters.
+
+print_other_rows([Row | Rest], RowNumber) :-
+    print_row_separator,
+    DisplayNumber is 8 - RowNumber,
+    format(' ~w ', [DisplayNumber]),
+    print_row(Row, RowNumber),
+    nl,
+    NextRowNumber is RowNumber + 1,
+    print_other_rows(Rest, NextRowNumber).
+
+print_row([], _) :-
+    write('|').
+
+print_row([Cell | Rest], RowNumber) :-
+    format('| ~w ', [Cell]),
+    print_row(Rest, RowNumber).
+
+print_board_separator :-
+    write('   +----+----+----+----+----+----+----+----+'),
+    nl.
+
+print_row_separator :-
+    write('   |----+----+----+----+----+----+----+----|'),
+    nl.
+
+print_bottom_letters :-
+    write('      A    B    C    D    E    F    G    H'),
+    nl.
+
+% para testar:
+% ?- initial_board(Board), print_board(Board).
--- Project1/utils.pl
+++ Project1/utils.pl
@@ -5,4 +5,4 @@ computer_difficulty(comp) :-
     format('Select a computer difficulty for ', [comp]), nl,
     write('1. Easy\n'),
     write('2. Medium\n'),
-    write('3. Difficult\n'),*/
+    write('3. Difficult\n'), */
