0c2307e40565a2047f0df992f6aca996a5d204a8
joao <up202106996@up.pt>
2023-10-31 16:04:13 +0000

    Help menu done

--- Project1/menu.pl
+++ Project1/menu.pl
@@ -56,6 +56,7 @@ process_choice(2) :-
 
 process_choice(3) :-
     write('Thanks for playing!\n'),
+    sleep(2),
     halt.
 
 display_play_menu :-
@@ -89,8 +90,22 @@ process_play_choice(_) :-
     display_play_menu.
 
 display_help_menu :-
-    % Just to visualize the help menu for now
-    write('Murus Gallicus is a game where players strategically conquer territories to defeat opponents.').
+    clear_console,
+    title,
+    write('\nDescription:\n\n'),
+    write('Murus Gallicus is a game where players strategically conquer territories to defeat opponents.\n\n'),
+    write('Rules and Notes:\n\n'),
+    write('1. Towers are formed by stacking two pieces (2 or II).\n'),
+    write('2. The decimal pieces start to play.\n'),
+    write('3. Only towers are allowed to move.\n'),
+    write('4. To move, select one of your towers and click two spaces away from it in any direction. One piece will move to the clicked spot, and the other stays in the middle space.\n'),
+    write('5. You cannot move to a space that already has two of your pieces or one opponent\'s piece (either a single piece or a tower).\n'),
+    write('6. If you have a tower with an isolated piece, moving it will result in one tower and one piece in the middle space.\n'),
+    write('7. If the opponent has an isolated piece next to one of your towers, you can sacrifice one of your pieces to "capture" the opponent\'s piece.\n'),
+    write('8. At no point in the game should one player have more pieces than the other. When one player loses a piece, the other must sacrifice one as well, maintaining balance.\n'),
+    write('9. A player wins under two conditions: by reaching the opponent\'s side of the board or by forcing a stalemate (where the opponent has no legal moves left).\n\n'),
+    write('You will be redirected to the main menu in 10 seconds. Enjoy the game!!\n'),
+    sleep(10).
 
 title :-
     write('   -----------------------------------------\n'),
@@ -139,7 +154,7 @@ game_cycle(GameState):-
     (validate(GameState, Col1-Row1, Col2-Row2) ->
         move(GameState, Col1-Row1-Col2-Row2, NewGameState),
         first_element(NewGameState, NewBoard),
-        % clear_console,
+        clear_console,
         title,
         print_board(NewBoard),
         game_cycle(NewGameState)
@@ -207,9 +222,6 @@ validate(GameState, ColI-RowI, ColF-RowF) :-
     targetAvailable(Board, ColF-RowF, Player),
 
     isTwoSquaresAway(ColI-RowI, ColF-RowF).
-
-    % Aqui falta verificar se a peça final avança, exatamente, duas casas
-    % e também se as moves são exatamente na diagonal (mas já tou farto disto)
 
 play :-
     gamestate(GameState), !,
